cbc3b6cd34c7763c62dd8b51760f13c60ba16054
Atabeg <atabeg_hamrayev@163.com>
2023-11-05 23:14:16 +0800

    Updated

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Heyyy");
+        Thread.sleep(2000);
+        System.out.println("How are you?");
+        Thread.sleep(2000);
+        System.out.println("I am good too :)");
+    }
+}
