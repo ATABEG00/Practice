a8857a2e43dbdada806cfafb4a94b49487349a04
Atabeg <atabeg_hamrayev@163.com>
2023-11-08 04:09:12 +0800

    Locat test updated

--- src/Test/LocalTest.java
+++ src/Test/LocalTest.java
@@ -2,7 +2,7 @@ package Test;
 
 public class LocalTest {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("This one is Local Test");
 
     }
 }
